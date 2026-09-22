eaa898ff54e3de7bdbae5bec1fef16fe5f908fe4
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2026-09-22 07:20:00 -0500

    Got the discord driver robot successfully working (completed ticket #10)

--- src/DiscordBot/DiscordDriver.ts
+++ src/DiscordBot/DiscordDriver.ts
@@ -1,28 +1,18 @@
 import { type IDriverEventInterface } from "../Driver.ts";
 import dotenv from "dotenv";
-import {
-	Client,
-	GatewayIntentBits,
-	ButtonBuilder,
-	ButtonStyle,
-	ModalBuilder,
-	TextInputBuilder,
-	TextInputStyle,
-	Events,
-	SlashCommandBuilder,
-	REST,
-	Routes,
-} from "discord.js";
+import { Client, GatewayIntentBits, WebhookClient } from "discord.js";
 
 import path from "node:path";
 import { messageCreateCommands } from "./Commands/MessageCreateCommands.ts";
 
 export class DiscordDriver implements IDriverEventInterface {
-	//Note client can't be a seperate member for some dam reason
+	//Different clients
+	static staticWebhookClient: WebhookClient;
+	static staticClient: Client;
 	StartupMethod(): boolean {
 		dotenv.config({});
 
-		const client = new Client({
+		DiscordDriver.staticClient = new Client({
 			intents: [
 				GatewayIntentBits.GuildMessages,
 				GatewayIntentBits.Guilds,
@@ -33,34 +23,16 @@ export class DiscordDriver implements IDriverEventInterface {
 			],
 		});
 
-		client.login(process.env.DISCORD_TOKEN);
-		client.on("clientReady", () => {
-			// let generalChannel = client.channels.cache.get(
-			// 	`${process.env.GENERAL_CHANNEL_TOKEN}`,
-			// );
-
-			// //Ignore this, for some reason it works
-			// DiscordDriver.#SendMessageToGeneral(client).send(
-			// 	`${DiscordDriver.#GetBotName(client)} is ready for service`,
-			// );
-
+		DiscordDriver.staticClient.login(process.env.DISCORD_TOKEN);
+		DiscordDriver.staticClient.on("clientReady", () => {
 			DiscordDriver.#SendMessageToGeneral(
-				client,
+				DiscordDriver.staticClient,
 				`${process.env.GENERAL_CHANNEL_TOKEN}`,
-				`${DiscordDriver.#GetBotName(client)} is ready for service`,
+				`${DiscordDriver.#GetBotName(DiscordDriver.staticClient)} is ready for service`,
 			);
 		});
 
-		// client.on(Events.InteractionCreate, async (interaction) => {
-		// 	if (interaction.customID === "Hello") {
-		// 		await interaction.reply({
-		// 			content: "Hello thing",
-		// 			ephemeral: true,
-		// 		});
-		// 	}
-		// });
-
-		client.on("messageCreate", async (message) => {
+		DiscordDriver.staticClient.on("messageCreate", async (message) => {
 			if (message.author.bot) {
 				return;
 			}
@@ -81,7 +53,29 @@ export class DiscordDriver implements IDriverEventInterface {
 
 		console.log("Discord Bot ready");
 
+		DiscordDriver.SendMessageToGeneral("Discord Response system is ready");
+		console.log("Discord Response system is ready");
+
 		return true;
+	}
+
+	static SendMessageToGeneral(incomingSendingString: string = "") {
+		// DiscordDriver.#SendMessageToGeneral(
+		// 	DiscordDriver.staticClient,
+		// 	process.env.GENERAL_CHANNEL_TOKEN,
+		// 	incomingSendingString,
+		// );
+
+		if (
+			DiscordDriver.staticWebhookClient == null ||
+			DiscordDriver.staticWebhookClient == undefined
+		) {
+			DiscordDriver.staticWebhookClient = new WebhookClient({
+				url: `${process.env.WEBHOOK_GENERAL_TOKEN}`,
+			});
+		}
+
+		DiscordDriver.staticWebhookClient.send(incomingSendingString);
 	}
 
 	async HeartbeatMethod(): Promise<boolean> {
@@ -109,6 +103,7 @@ export class DiscordDriver implements IDriverEventInterface {
 			`${channelToken}`,
 		);
 		if (generalChannel !== undefined) {
+			//! Doesn't cause problems. Send is a general channel method
 			generalChannel.send(message);
 		}
 	}
