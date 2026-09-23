6758b3d06b529233208742333de55120e66ec9b7
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2026-09-23 07:57:02 -0500

    Added the services handlers scripts

---
+++ src/ServicesHandler/ServicesHandler.test.ts
@@ -0,0 +1,5 @@
+import { expect, describe, test } from "vitest";
+
+describe("Ping Scout/Ping Range Tests", () => {});
+describe("Sending commands to services tests", () => {});
+describe("Recieving notifications tests", () => {});
